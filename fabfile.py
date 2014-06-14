88a30eddf6cdb255a3f921ee54353c831ec9e57a
Francis Giraldeau <francis.giraldeau@gmail.com>
2014-06-14 15:14:58 -0400

    Skeleton for new web site
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

---
+++ fabfile.py
@@ -0,0 +1,15 @@
+from fabric.api import local, run, env, lcd, cd, sudo
+from fabric.operations import put
+from fabric.context_managers import shell_env, prefix
+from fabric.decorators import with_settings
+
+env.hosts = ['step.polymtl.ca']
+env.user = 'fgiraldeau'
+
+homedir = '/home/fgiraldeau/'
+basedir = homedir + 'nova_html'
+
+def deploy():
+    with cd(basedir):
+        run("git pull origin master")
+
